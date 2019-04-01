9c383cc096e274d7224738115e5bf4733345f6b5
zhangshaolong <zhangshaolong@didichuxing.com>
2019-04-01 19:29:37 +0800

    support plugins for loader

--- src/module/loader.js
+++ src/module/loader.js
@@ -3,6 +3,8 @@ import { moduleTag } from '../config'
 
 let moduleLoader = null
 
+const pluginConfig = {}
+
 const modulesMap = {}
 
 const tagName = moduleTag.toUpperCase()
@@ -17,15 +19,37 @@ const getParentModule = (node) => {
   }
 }
 
-const setLoader = (loader) => {
+const setLoader = (loader, config) => {
   moduleLoader = loader
+  if (config) {
+    for (let idx in config) {
+      pluginConfig[idx] = config[idx]
+    }
+  }
 }
 
 const loadModule = (path, container) => {
   if (!moduleLoader) {
     return
   }
-  return Promise.all(moduleLoader(path)).then((values) => {
+  let files = moduleLoader(path)
+  let plugins = []
+  let syncs = files.slice(0, 2)
+  if (files.length > 2) {
+    for (let i = 2; i < files.length; i++) {
+      let file = files[i]
+      let type = 'sync'
+      if (file.constructor === Promise) {
+        syncs.push(file)
+        type = 'async'
+      }
+      plugins.push({
+        type,
+        file
+      })
+    }
+  }
+  return Promise.all(syncs).then((values) => {
     let props = container.getAttribute('c-props')
     if (props) {
       props = JSON.parse(props)
@@ -49,6 +73,18 @@ const loadModule = (path, container) => {
     module.container = container
     module.tpl = values[1].default
     container.module = module
+    let offset = 0
+    plugins.forEach((pluginObj, i) => {
+      if (pluginConfig[i + 2]) {
+        let plugin = pluginObj.file
+        if (pluginObj.type === 'async') {
+          plugin = values[i + 2 - offset].default
+        } else {
+          offset++
+        }
+        module[pluginConfig[i + 2]] = plugin
+      }
+    })
     return module.init()
   })
 }
